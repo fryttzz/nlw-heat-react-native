225d4b9bfa0ec57b3e83521455dd54753e6b1ee3
Matheus Santos <matheusps0012@gmail.com>
2021-10-22 18:08:22 -0300

    Add - Header And SVGs

---
+++ src/components/UserPhoto/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import { COLORS } from '../../theme'
+export const styles = StyleSheet.create({
+    avatar: {
+        borderWidth: 4,
+        borderColor: COLORS.BLACK_SECONDARY
+    },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
+import { Header } from "../../components/Header";
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá React Native</Text>
+      <Header />
     </View>
   );
 }
