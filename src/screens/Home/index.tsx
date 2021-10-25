b8a9d439635878b18389bc3a87727a5604e8c7f8
Matheus Santos <matheusps0012@gmail.com>
2021-10-25 10:59:42 -0300

    Add - Send Message Form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,13 +4,14 @@ import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
