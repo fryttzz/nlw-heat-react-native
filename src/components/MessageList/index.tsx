19cd752db475fbed893faaa2de93df6a0baf6f29
Matheus Santos <matheusps0012@gmail.com>
2021-10-25 10:15:42 -0300

    Add - Message and Message List

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { styles } from "./styles";
+import { Message } from "../Message";
+export function MessageList() {
+  const message = {
+    id: "1",
+    text: "Mensagem de teste",
+    user: {
+      name: "Matheus",
+      avatar_url: "https://github.com/fryttzz.png",
+    },
+  };
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+    </ScrollView>
+  );
+}
